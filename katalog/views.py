84f47510dfcee2987964b3f563a721aedc9fb8ee
yudiptr <yudiputrasabri789@gmail.com>
2022-09-10 15:23:45 +0700

    first try

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_katalog,
+    'nama': 'Yudi :D',
+    'npm': 2106706123,
+}
