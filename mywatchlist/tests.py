48ffb5e7dbd57e8231ceb50fbe7a2f191246e8fc
yudiptr <94793172+yudiptr@users.noreply.github.com>
2022-09-28 22:10:12 +0700

    ubah dikit hehe

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -10,10 +10,10 @@ class TestViews(TestCase):
     
     def test_show_xml_resolved(self):         
         client = Client()
-        response = client.get(reverse("mywatchlist:show_mywatchlist_html"))
+        response = client.get(reverse("mywatchlist:show_mywatchlist_xml"))
         self.assertEquals(response.status_code,200)
     
     def test_show_json_resolved(self):         
         client = Client()
-        response = client.get(reverse("mywatchlist:show_mywatchlist_html"))
+        response = client.get(reverse("mywatchlist:show_mywatchlist_json"))
         self.assertEquals(response.status_code,200)
